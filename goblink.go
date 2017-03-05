e27ad92df0d4177adcc51292deb87ce6149f8d9c
Ben Rockwood <benr@joyent.com>
2017-03-05 01:29:58 -0800

    Basic blink test using go-rpio

--- goblink.go
+++ goblink.go
@@ -2,10 +2,29 @@ package main
 
 import (
   "fmt"
+  "time"
+  "os"
+  import "github.com/stianeikeland/go-rpio"
 )
 
 func main() {
 
-  fmt.Println("Hello.");
+  fmt.Println("Starting blinker...");
+  err := rpio.Open();
+  if (err !+ nil){
+    fmt.Println(err);
+    os.Exit(1);
+  }
+
+  defer rpio.Close();
+  
+  pin := rpio.Pin(14); // GPIO14, Physical Pin 8 (Use GND on Pin 6)
+  pin.Output();
+  pin.PullDown();
+
+  for {
+    time.Sleep(1000 * time.Millisecond); 
+    pin.Toggle();
+  } 
 
 }
